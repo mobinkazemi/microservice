727fbdd460bda5966c5c5e098e4795a9c9c8ac9f
mobinkazemi <s.mobinkazemi@gmail.com>
2025-07-26 16:45:37 +0330

    comments - feat - implemented

---
+++ 1.postsAndComments/comments/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  const postId = req.params.id;
+
+  res.send(commentsByPostId[postId] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const postId = req.params.id;
+
+  const { content } = req.body;
+
+  const comments = commentsByPostId[postId] || [];
+
+  comments.push({ id, content });
+
+  commentsByPostId[postId] = comments;
+
+  res.status(201).send(commentsByPostId[postId]);
+});
+
+app.listen(4001, () => console.log("Server is running on 4001"));
