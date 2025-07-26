8a0f63d1de67e1c180d306e31e3b674804d011c8
mobinkazemi <s.mobinkazemi@gmail.com>
2025-07-26 17:36:15 +0330

    client - feat - post list implemented

--- 1.postsAndComments/client/src/App.js
+++ 1.postsAndComments/client/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 
-export default () => {
-  return (
-    <div className="container">
-      <h1>Create Post</h1>
-      <PostCreate></PostCreate>
-    </div>
-  );
-};
+export default () => (
+  <div className="container">
+    <h1>Create Post</h1>
+    <PostCreate></PostCreate>
+    <hr></hr>
+
+    <h1>List of Posts</h1>
+    <PostList />
+  </div>
+);
---
+++ 1.postsAndComments/client/src/PostList.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const PostList = () => {
+  const [postList, setPostList] = useState({});
+
+  const fetchPost = async () => {
+    const res = await axios.get("http://localhost:4000/posts");
+
+    setPostList(res.data);
+  };
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  const posts = Object.values(postList).map((p) => {
+    return (
+      <div
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+        key={p.id}
+      >
+        <div className="card-body">
+          <h3>{p.title}</h3>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {posts}
+    </div>
+  );
+};
+
+export default PostList;
