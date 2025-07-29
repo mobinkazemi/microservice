b9b809609964f0b0a48d4adf634ec1f7b0602536
mobinkazemi <s.mobinkazemi@gmail.com>
2025-07-29 15:54:35 +0330

    event bus - feat - event saved in array to handle downtime

--- 1.postsAndComments/event-bus/index.js
+++ 1.postsAndComments/event-bus/index.js
@@ -6,25 +6,33 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
-  const data = req.body;
+  const event = req.body;
+
+  events.push(event);
 
   axios
-    .post("http://localhost:4000/events", data)
+    .post("http://localhost:4000/events", event)
     .catch((el) => console.log(el));
   axios
-    .post("http://localhost:4001/events", data)
+    .post("http://localhost:4001/events", event)
     .catch((el) => console.log(el));
   axios
-    .post("http://localhost:4002/events", data)
+    .post("http://localhost:4002/events", event)
     .catch((el) => console.log(el));
   axios
-    .post("http://localhost:4003/events", data)
+    .post("http://localhost:4003/events", event)
     .catch((el) => console.log(el));
 
-  console.log("Broadcasting a new event:", data.type);
+  console.log("Broadcasting a new event:", event.type);
 
   return res.status(200).send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  return res.send(events);
 });
 
 app.listen(4005, () => console.log("Event-Bus is running on 4005"));
--- 1.postsAndComments/query/index.js
+++ 1.postsAndComments/query/index.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 const posts = {};
 
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
-
+function eventHandler(type, data) {
   if (type === "PostCreated") {
     posts[data.id] = {
       id: data.id,
@@ -30,8 +29,13 @@ app.post("/events", (req, res) => {
       }
     });
   }
+}
 
-  console.dir(posts, { depth: null });
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  eventHandler(type, data);
+
   return res.send({});
 });
 
@@ -39,4 +43,13 @@ app.get("/posts", (req, res) => {
   return res.send(posts);
 });
 
-app.listen(4002, () => console.log("Query is running on 4002"));
+app.listen(4002, async () => {
+  console.log("Query is running on 4002");
+
+  const events = await axios.get("http://localhost:4005/events");
+
+  for (let ev of events.data) {
+    console.log(ev);
+    eventHandler(ev.type, ev.data);
+  }
+});
