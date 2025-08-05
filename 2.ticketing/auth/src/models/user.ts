a917798f0421900ab3dc8688f94444c372f043c4
mobinkazemi <s.mobinkazemi@gmail.com>
2025-08-05 17:44:17 +0330

    auth: models - feat - user model added

---
+++ 2.ticketing/auth/src/models/user.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+interface IUser {
+    email: string;
+    password: string;
+}
+
+interface IUserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+}
+
+interface IUserSchema extends mongoose.Model<IUserDoc> {
+    build(data: IUser): IUserDoc;
+}
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+});
+
+const User = mongoose.model<IUserDoc, IUserSchema>("User", userSchema);
+
+userSchema.statics.build = (data: IUser) => {
+    return new User(data);
+};
+
+
+export { User };
