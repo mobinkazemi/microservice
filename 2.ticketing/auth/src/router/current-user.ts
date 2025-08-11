6092f6d9d9ca23f837639b5824e188f95df8c981
mobinkazemi <s.mobinkazemi@gmail.com>
2025-08-11 20:38:26 +0330

    auth - feat - require auth middleware implemented

--- 2.ticketing/auth/src/router/current-user.ts
+++ 2.ticketing/auth/src/router/current-user.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { currentUserMiddleware } from "../middleware/currentUser";
+import { requireAuth } from "../middleware/require-auth";
 
 const router = express.Router();
 
-router.get("/api/users/current-user", currentUserMiddleware, (req: Request, res: Response) => {
+router.get("/api/users/current-user", currentUserMiddleware, requireAuth, (req: Request, res: Response) => {
     return res.send({ currentUser: req.currentUser || null })
 });
 
