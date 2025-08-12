f9df5fec957e827db1e8a6cc81bbd17eb0ee5f92
mobinkazemi <s.mobinkazemi@gmail.com>
2025-08-12 15:40:07 +0330

    auth - refactor - app definition separated from running app into 2 different files (index.ts, app.ts)

---
+++ 2.ticketing/auth/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+import { json } from 'body-parser'
+import { errorHandlerMiddleware } from './middleware/error-handler'
+import { RouteNotFoundError } from './errors/route-not-found.error'
+import mongoose from 'mongoose'
+import { signupRouter } from './router/signup'
+import cookieSession from 'cookie-session'
+import { signinRouter } from './router/signin'
+import { currentUserRouter } from './router/current-user'
+import { signoutRouter } from './router/signout'
+
+const app = express()
+app.set('trust proxy', true)
+
+app.use(json())
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true,
+    })
+)
+app.use(signupRouter)
+app.use(signinRouter)
+app.use(currentUserRouter)
+app.use(signoutRouter)
+
+app.all(/(.*)/, async () => {
+    throw new RouteNotFoundError()
+})
+
+app.use(errorHandlerMiddleware)
+
+export { app }
--- 2.ticketing/auth/src/index.ts
+++ 2.ticketing/auth/src/index.ts
@@ -1,45 +1,15 @@
-import express from 'express'
-import { json } from 'body-parser'
-import { errorHandlerMiddleware } from './middleware/error-handler'
-import { RouteNotFoundError } from './errors/route-not-found.error'
-import mongoose from 'mongoose'
-import { signupRouter } from './router/signup'
-import cookieSession from 'cookie-session'
-import { signinRouter } from './router/signin'
-import { currentUserRouter } from './router/current-user'
-import { signoutRouter } from './router/signout'
-
-const app = express()
-app.set('trust proxy', true)
-
-app.use(json())
-app.use(
-    cookieSession({
-        signed: false,
-        secure: true,
-    })
-)
-app.use(signupRouter)
-app.use(signinRouter)
-app.use(currentUserRouter)
-app.use(signoutRouter)
-
-app.all(/(.*)/, async () => {
-    throw new RouteNotFoundError()
-})
-
-app.use(errorHandlerMiddleware)
+import mongoose from "mongoose";
+import { app } from "./app";
 
 app.listen(3000, async () => {
-
     if (!process.env.JWT_KEY) {
-        throw new Error('JWT_KEY env not defined')
+        throw new Error("JWT_KEY env not defined");
     }
     try {
-        await mongoose.connect('mongodb://auth-mongo-clusterip-srv:27017/auth')
-        console.log('Connected to MongoDB')
+        await mongoose.connect("mongodb://auth-mongo-clusterip-srv:27017/auth");
+        console.log("Connected to MongoDB");
     } catch (error) {
-        console.error('Error connecting to MongoDB:', error)
+        console.error("Error connecting to MongoDB:", error);
     }
-    console.log('Auth is running on port 3000!!')
-})
+    console.log("Auth is running on port 3000!!");
+});
