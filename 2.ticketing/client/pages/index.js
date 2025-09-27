c400f78e0ccd88ab9cc6c6c50c3e0ff143cd6265
mobinkazemi <s.mobinkazemi@gmail.com>
2025-09-27 17:11:30 +0330

    ticket: auth - feat - detecting SSR and handling Cookies implemented

--- 2.ticketing/client/pages/index.js
+++ 2.ticketing/client/pages/index.js
@@ -1,5 +1,34 @@
-const landing = () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log({ currentUser });
   return <h1>landing page</h1>;
 };
 
-export default landing;
+LandingPage.getInitialProps = async ({ req }) => {
+  try {
+    let response;
+
+    // This is how we figure out that if requester is from browser or from inside the pod (Nodejs Pod environment)
+    if (typeof window === "undefined") {
+      // So we are on Pod Nodejs env
+      const url =
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local" +
+        "/api/users/current-user";
+
+      response = await axios.get(url, { headers: req.headers });
+    } else {
+      // we are on browser
+      const url = "/api/users/current-user";
+
+      response = await axios.get(url);
+    }
+
+    return { currentUser: response.data.currentUser };
+  } catch (error) {
+    console.log(error.message);
+    return { currentUser: null };
+  }
+};
+
+export default LandingPage;
