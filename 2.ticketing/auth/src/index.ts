41510a7889e6a72abca2de5d15fa39df63a509e1
mobinkazemi <s.mobinkazemi@gmail.com>
2025-10-08 10:20:49 +0330

    ticket: auth - refactor -  db url moved to infra yaml file

--- 2.ticketing/auth/src/index.ts
+++ 2.ticketing/auth/src/index.ts
@@ -5,8 +5,11 @@ app.listen(3000, async () => {
     if (!process.env.JWT_KEY) {
         throw new Error("JWT_KEY env not defined");
     }
+    if (!process.env.MONGO_URI) {
+        throw new Error("MONGO_URI env not defined")
+    }
     try {
-        await mongoose.connect("mongodb://auth-mongo-clusterip-srv:27017/auth");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
