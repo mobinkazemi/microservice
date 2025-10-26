fa609570da1f1cfc9c95ac8a1569fc6ed6f2d441
mobinkazemi <s.mobinkazemi@gmail.com>
2025-10-26 13:39:54 +0330

    Common - feat - Update

--- 2.ticketing/common/src/index.ts
+++ 2.ticketing/common/src/index.ts
@@ -4,6 +4,7 @@ export * from './errors/database-connection.error'
 export * from './errors/not-authorized.error'
 export * from './errors/request-validation.error'
 export * from './errors/route-not-found.error'
+export * from './errors/resource-not-found.error'
 
 export * from './middleware/currentUser'
 export * from './middleware/error-handler'
